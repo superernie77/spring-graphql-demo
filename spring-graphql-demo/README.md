32d48e45f15fff9cb64470a88844ee557cadb1ff
superernie77 <superernie77@gmail.com>
2022-11-16 10:40:48 +0100

    simple endpoint added

---
+++ spring-graphql-demo/README.md
@@ -0,0 +1,13 @@
+# Spring-Boot Graphql Demo Application
+
+Exposes a single graphql endpoint through the Spring-Graphql integration module.
+URL of Graphql console: http://localhost:8080/graphiq
+
+try the following query:
+```
+query {
+  customers {
+    id
+  }
+}
+```
--- spring-graphql-demo/src/main/java/se77/springgraphqldemo/SpringGraphqlDemoApplication.java
+++ spring-graphql-demo/src/main/java/se77/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -1,7 +1,15 @@
 package se77.springgraphqldemo;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import org.springframework.stereotype.Service;
+
+import graphql.schema.idl.RuntimeWiring.Builder;
 
 @SpringBootApplication
 public class SpringGraphqlDemoApplication {
@@ -10,4 +18,42 @@ public class SpringGraphqlDemoApplication {
 		SpringApplication.run(SpringGraphqlDemoApplication.class, args);
 	}
 
+	@Bean
+	RuntimeWiringConfigurer runtimeWiringConf(CrmService service) {
+		return new RuntimeWiringConfigurer() {
+
+			@Override
+			public void configure(Builder builder) {
+				
+				builder.type("Customer", wiring ->
+					wiring.dataFetcher("profile", e -> service.getProfileFor(e.getSource())));
+
+				builder.type("Query", wiring -> 
+					wiring.dataFetcher("customersById", env -> service.getCustomerById(env.getArgument("id")))
+						.dataFetcher("customers", env -> service.getCustomers()));
+			}
+		};
+	}
+
+}
+
+record Customer(Integer id, String name) {
+};
+
+record Profile (Integer id, Integer CustomerId) {};
+
+@Service
+class CrmService {
+	
+	public Profile getProfileFor(Customer customer) {
+		return new Profile(customer.id(), customer.id());
+	}
+
+	public Customer getCustomerById(Integer id) {
+		return new Customer(id, "Ernie");
+	}
+	
+	public Collection<Customer> getCustomers() {
+		return List.of(new Customer(1, "Ernie"), new Customer(1, "Bert"));
+	}
 }
